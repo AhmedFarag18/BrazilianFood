3a4d3ffd0ef3438baed84b855ac069998f78c943
AhmedFarag18 <73128333+AhmedFarag18@users.noreply.github.com>
2021-01-29 09:29:12 +0200

    filesWebsite

---
+++ js/index.js
@@ -0,0 +1,71 @@
+$(document).ready(function() {
+
+
+    // //slider height
+    // $(".owl-carousel .item").height($(window).height())
+    // $(window).resize(function() {
+    //     $('.owl-carousel .item').height($(window).height())
+    // })
+
+
+    // Owl carousel
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        items: 1,
+        autoplay: true,
+        animateOut: 'fadeOut',
+        smartSpeed: 450,
+    })
+
+
+    //side-menu
+    $('.menu').on('click', function() {
+        $('.side-menu').toggleClass('open')
+    })
+
+    $('.fa-times').on('click', function() {
+        $('.side-menu').removeClass('open')
+    })
+
+    //scroll navbar
+    $(window).scroll(function() {
+        if ($(this).scrollTop() >= 30) {
+            $('.nav-left').fadeOut(500);
+            $('.nav-top').fadeIn(500);
+        } else {
+            $('.nav-left').fadeIn(500);
+            $('.nav-top').fadeOut(500);
+        }
+    })
+
+
+    // Counter up
+    const counters = document.querySelectorAll('.about .num');
+    const delay = 100;
+
+    counters.forEach(counter => {
+        const updateCount = () => {
+            const target = counter.getAttribute('data-target')
+            const count = +counter.innerText;
+            const speed = target / delay;
+
+
+            if (count < target) {
+
+                counter.innerText = Math.ceil(count + speed);
+                setTimeout(updateCount, 1);
+
+            } else {
+                counter.innerText = target;
+            }
+        }
+        updateCount();
+    })
+
+    // count down
+    var content = $('.count-down').html();
+
+    $('.count-down').countdown('2021/03/28', function(event) {
+        $(this).html(event.strftime(content));
+    });
+})
